dd3db9c19aab36b0ecbdd772075199e0da67791f
vg-prep-code <venkatesh.galla12@gmail.com>
2024-07-03 17:54:47 +0530

    batch task fix

--- src/main/java/com/vg/tradestore/TradeStoreApplication.java
+++ src/main/java/com/vg/tradestore/TradeStoreApplication.java
@@ -3,9 +3,11 @@ package com.vg.tradestore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ComponentScan({"com.vg.tradestore"})
+@EnableScheduling
 public class TradeStoreApplication {
 
 	public static void main(String[] args) {
